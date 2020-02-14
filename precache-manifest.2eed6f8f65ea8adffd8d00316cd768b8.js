bda6f051d11abefa3ef6a97ad7eea07d96b80078
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-14 09:23:20 +0000

    Deploy seladb/StarTrack-js to github.com/seladb/StarTrack-js.git:gh-pages

---
+++ precache-manifest.2eed6f8f65ea8adffd8d00316cd768b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abae043ada3b6604886e68a0e7a2d8ab",
+    "url": "/StarTrack-js/index.html"
+  },
+  {
+    "revision": "4721cc58ce400ac1e3eb",
+    "url": "/StarTrack-js/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9edc449a34050707c273",
+    "url": "/StarTrack-js/static/css/main.965d24cf.chunk.css"
+  },
+  {
+    "revision": "4721cc58ce400ac1e3eb",
+    "url": "/StarTrack-js/static/js/2.919c0b7a.chunk.js"
+  },
+  {
+    "revision": "9edc449a34050707c273",
+    "url": "/StarTrack-js/static/js/main.8f7569f3.chunk.js"
+  },
+  {
+    "revision": "9ff90c49aadeb9f51e44",
+    "url": "/StarTrack-js/static/js/runtime-main.f683f26e.js"
+  }
+]);
